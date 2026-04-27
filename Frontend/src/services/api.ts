ac5c3eefca8464fab6cfd3f8044c84d753bd179d
MathisP <mathixx59@outlook.fr>
2026-04-27 11:55:40 +0200

    V7 Final

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Créer une instance axios
 const api = axios.create({
